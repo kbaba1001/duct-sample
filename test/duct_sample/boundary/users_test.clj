89c35142bb0a7ff4b7305f25639b1542df43f340
kbaba1001 <kbaba1001@users.noreply.github.com>
2018-02-02 23:46:55 +0900

    add memo

--- test/duct_sample/boundary/users_test.clj
+++ test/duct_sample/boundary/users_test.clj
@@ -16,6 +16,8 @@
     (println (users/get-users db)) ; テストを実行
     (jdbc/delete! db-spec :users []))) ; データの消去
 
+; TODO eftestはパラレル実行なので、ひとまずシングル実行にしておきたい
+; TODO before, after 的な処理の書き方を知りたい。
 
 ; (deftest get-users-test
 ;   (testing "get users"
