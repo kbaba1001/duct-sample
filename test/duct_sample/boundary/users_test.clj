4ed8791b060d97cfe6cf4cb62966745fc7b3e5ce
kbaba1001 <kbaba1001@users.noreply.github.com>
2018-02-02 23:28:10 +0900

    DBを使ったテスト

--- test/duct_sample/boundary/users_test.clj
+++ test/duct_sample/boundary/users_test.clj
@@ -1,16 +1,20 @@
 (ns duct-sample.boundary.users-test
   (:require [clojure.test :refer :all]
+            [clojure.java.jdbc :as jdbc]
             [integrant.core :as ig]
             [duct-sample.boundary.users :as users]
             [fipp.edn :refer (pprint) :rename {pprint fipp}])
   (:use [duct.database.sql]))
 
-(def db-spec "jdbc:postgresql://localhost:5432/duct-sample-test?user=postgres&password=")
+(def db-spec "jdbc:postgresql://localhost:5432/duct-sample-test?user=postgres")
 
 ; これでSQLを実際のデータベースで実行できる。
 ; use ではなく require だと -> がうまく出来なかった
 (let [db (->Boundary db-spec)]
-  (println (users/get-users db)))
+  (do
+    (users/create-user db {:name "kbaba" :email "kbaba@example.com"}) ; データの作成
+    (println (users/get-users db)) ; テストを実行
+    (jdbc/delete! db-spec :users []))) ; データの消去
 
 
 ; (deftest get-users-test
