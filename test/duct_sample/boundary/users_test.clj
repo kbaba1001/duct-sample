13e4c3693230f15e3d264827c85f9345cd87f65c
kbaba1001 <kbaba1001@users.noreply.github.com>
2018-02-08 22:56:08 +0900

    wip テストを書く

--- test/duct_sample/boundary/users_test.clj
+++ test/duct_sample/boundary/users_test.clj
@@ -8,13 +8,50 @@
 
 (def db-spec "jdbc:postgresql://localhost:5432/duct-sample-test?user=postgres")
 
+; (defn db []
+;   (->Boundary db-spec))
+
+; (defn create-data []
+;   (do
+;     (println "create-data")
+;     (users/create-user db {:name "kbaba" :email "kbaba@example.com"})))
+
+; (defn delete-data []
+;   (do
+;     (println "delete-test")
+;     (jdbc/delete! db-spec :users [])))
+
+; (defn my-test-fixture [f]
+;         (create-data)
+;         (f)
+;         (delete-data))
+
+; (use-fixtures :once my-test-fixture)
+
 ; これでSQLを実際のデータベースで実行できる。
 ; use ではなく require だと -> がうまく出来なかった
-(let [db (->Boundary db-spec)]
-  (do
-    (users/create-user db {:name "kbaba" :email "kbaba@example.com"}) ; データの作成
-    (println (users/get-users db)) ; テストを実行
-    (jdbc/delete! db-spec :users []))) ; データの消去
+(deftest test-get-users
+  (testing "get users"
+    (let [db (->Boundary db-spec)]
+      (do
+        (users/create-user db {:name "kbaba" :email "kbaba@example.com"}) ; データの作成
+        (is (= 5 (users/get-users db)))     ; テストを実行
+        (jdbc/delete! db-spec :users []))) ; データの消去
+    ))
+
+; (let [db (->Boundary db-spec)]
+;   (do
+;     (users/create-user db {:name "kbaba" :email "kbaba@example.com"}) ; データの作成
+;     (println (users/get-users db))     ; テストを実行
+;     (jdbc/delete! db-spec :users []))) ; データの消去
+
+; (deftest test-get-users
+;   (testing "get users"
+;     (do
+;       (println "test-get-users")
+;       (println (users/get-users db)))))
+
+
 
 ; TODO eftestはパラレル実行なので、ひとまずシングル実行にしておきたい
 ; TODO before, after 的な処理の書き方を知りたい。
