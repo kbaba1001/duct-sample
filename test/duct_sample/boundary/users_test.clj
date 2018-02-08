e46688cd99e7833a1d88d63274f2a9563d3f90e4
kbaba1001 <kbaba1001@users.noreply.github.com>
2018-02-08 23:58:22 +0900

    setupとcleanupを利用したテスト

--- test/duct_sample/boundary/users_test.clj
+++ test/duct_sample/boundary/users_test.clj
@@ -8,57 +8,30 @@
 
 (def db-spec "jdbc:postgresql://localhost:5432/duct-sample-test?user=postgres")
 
-; (defn db []
-;   (->Boundary db-spec))
-
-; (defn create-data []
-;   (do
-;     (println "create-data")
-;     (users/create-user db {:name "kbaba" :email "kbaba@example.com"})))
-
-; (defn delete-data []
-;   (do
-;     (println "delete-test")
-;     (jdbc/delete! db-spec :users [])))
-
-; (defn my-test-fixture [f]
-;         (create-data)
-;         (f)
-;         (delete-data))
-
-; (use-fixtures :once my-test-fixture)
-
 ; これでSQLを実際のデータベースで実行できる。
 ; use ではなく require だと -> がうまく出来なかった
+(def db (->Boundary db-spec))
+
+(defn create-data []
+  (do
+    (println "create-data")
+    (users/create-user db {:name "kbaba" :email "kbaba@example.com"})))
+
+(defn delete-data []
+  (do
+    (println "delete-test")
+    (jdbc/delete! db-spec :users [])))
+
+(defn my-test-fixture [f]
+        (create-data)
+        (f)
+        (delete-data))
+
+(use-fixtures :once my-test-fixture)
+
 (deftest test-get-users
   (testing "get users"
-    (let [db (->Boundary db-spec)]
-      (do
-        (users/create-user db {:name "kbaba" :email "kbaba@example.com"}) ; データの作成
-        (is (= 5 (users/get-users db)))     ; テストを実行
-        (jdbc/delete! db-spec :users []))) ; データの消去
-    ))
-
-; (let [db (->Boundary db-spec)]
-;   (do
-;     (users/create-user db {:name "kbaba" :email "kbaba@example.com"}) ; データの作成
-;     (println (users/get-users db))     ; テストを実行
-;     (jdbc/delete! db-spec :users []))) ; データの消去
-
-; (deftest test-get-users
-;   (testing "get users"
-;     (do
-;       (println "test-get-users")
-;       (println (users/get-users db)))))
-
-
-
-; TODO eftestはパラレル実行なので、ひとまずシングル実行にしておきたい
-; TODO before, after 的な処理の書き方を知りたい。
-
-; (deftest get-users-test
-;   (testing "get users"
-;     (let [db (ig/ref :duct.database/sql)]
-;       (is (= "aaa" (users/get-users db))))))
-
-; (users/get-users db)
+    (let [user-data (first (users/get-users db))]
+      (is (= "kbaba@example.com" (:email user-data))
+      (is (= "kbaba" (:name user-data)))
+    ))))
